836912aacf8a3042e3e3a6d2f6fe0e6d0b0d3439
marek2222 <marekmusial2@o2.pl>
2018-08-14 23:56:07 +0200

    vuejs2-playlist: 02.04: Data Binding

--- vuejs2-playlist/app.js
+++ vuejs2-playlist/app.js
@@ -2,7 +2,9 @@ new Vue({
   el: '#vue-app',
   data: {
     name: 'marek2222',
-    job: 'ninja'
+    job: 'ninja',
+    website: 'http://thenetninja.co.uk',
+    websiteTag: '<a href="http://thenetninja.co.uk">The Net Ninja Website</a>'
   },
   methods: {
     greet: function(time) {
