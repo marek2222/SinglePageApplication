65b819e21e16c43d45a5fbba0b4c39026af6a44f
marek2222 <marekmusial2@o2.pl>
2018-08-14 22:43:55 +0200

    vuejs2-playlist: 02.03: Data

--- vuejs2-playlist/app.js
+++ vuejs2-playlist/app.js
@@ -1,6 +1,12 @@
 new Vue({
   el: '#vue-app',
   data: {
-    name: 'marek2222'
+    name: 'marek2222',
+    job: 'ninja'
+  },
+  methods: {
+    greet: function(time) {
+      return 'Good '+ time + ' '+ this.name;
+    }
   }
 });
