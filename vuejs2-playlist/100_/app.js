87b83aceb5cb8253a1aedca0106bad730a14fafe
marek2222 <marekmusial2@o2.pl>
2018-08-22 09:07:29 +0200

    vuejs2-playlist: 02.13: Simple punchbag game. Not well doing in this version Vue.

--- vuejs2-playlist/100_/app.js
+++ vuejs2-playlist/100_/app.js
@@ -1,14 +1,12 @@
 new Vue({
   el: '#vue-app',
   data: {
-    // b: 0
+  
   },
   methods: {
+  
   },
   computed: {
-    // addToB: function() {
-    //   console.log('addToB');
-    //   return this.b + this.age;
-    // }
+  
   }
 });
