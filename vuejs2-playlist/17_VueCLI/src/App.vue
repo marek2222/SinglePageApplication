1a67488ce559da39fad86fe14b8f7907b1897cc9
marek2222 <marekmusial2@o2.pl>
2018-08-24 08:27:12 +0200

    vuejs2-playlist: 02.19:  Nesting & importing components - GLOBALLY.      Base on the 17_VueCLI.

--- vuejs2-playlist/17_VueCLI/src/App.vue
+++ vuejs2-playlist/17_VueCLI/src/App.vue
@@ -1,23 +1,50 @@
  <template>
   <div>
     <h1>{{ tytul }}</h1>
+    <p><b> Globally</b><br>
+    Inside main.js should be:
+    <br> 1)    import Ninjas from './Ninjas.vue'     and
+   <br>  2)    Vue.component ('ninjas', Ninjas);</p>
+    <p><b> Locally</b><br>
+    Inside App.vue in section &ltscript&gt should be:
+    <br> 1)    import Ninjas from './Ninjas.vue'     and
+   <br>  2)
+   export default {
+    components: {
+     'ninjas': Ninjas
+    },
+    ...another code
+</p>
+   <br>
+    <p>List from globally 'Ninjas.vue'</p>
     <ninjas></ninjas>
   </div>
 </template>
 
 <script>
-import Ninjas from './Ninjas.vue'
-
+//globally component
 export default {
- components: {
-   'ninjas': Ninjas
- },
  data() {
     return {
-      tytul: 'Ninja App'
+      tytul: 'Ninja App - globally component'
     }
   }
 }
+
+////  locally component
+// import Ninjas from './Ninjas.vue'
+
+// export default {
+//  components: {
+//    'ninjas': Ninjas
+//  },
+//  data() {
+//     return {
+//       tytul: 'Ninja App - locally component'
+//     }
+//   }
+// }
+
 </script>
 
 <style>
