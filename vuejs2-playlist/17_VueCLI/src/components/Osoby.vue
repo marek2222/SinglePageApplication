86e4ea0e5300351086c04efae93afd4474d7265b
marek2222 <marekmusial2@o2.pl>
2018-08-25 22:06:00 +0200

    vuejs2-playlist: 02.26:  Lifecycle hooks.    		Base on the 17_VueCLI.

--- vuejs2-playlist/17_VueCLI/src/components/Osoby.vue
+++ vuejs2-playlist/17_VueCLI/src/components/Osoby.vue
@@ -2,10 +2,10 @@
     <div id="osoby ">
         <!-- <p>{{ osoby }}</p> -->
         <ul>
-        <li v-for="osoba in osoby" @click="osoba.widocznosc =!  osoba.widocznosc">
-            <h2>{{ osoba.nazwa }}</h2>
-            <h3 v-show=" osoba.widocznosc">{{ osoba.specjalnosc }}</h3>
-        </li>
+            <li v-for="osoba in osoby" @click="osoba.widocznosc =!  osoba.widocznosc">
+                <h2>{{ osoba.nazwa }}</h2>
+                <h3 v-show=" osoba.widocznosc">{{ osoba.specjalnosc }}</h3>
+            </li>
         </ul>
         <button @click="usunOsobe">Usun osobę </button>
     </div>
@@ -26,32 +26,52 @@ export default {
     },
     methods: {
         usunOsobe: function () {
-      this.osoby.pop();
-      }
+            this.osoby.pop();
+        }
+    },
+    //lifecycle hooks
+    beforeCreate(){
+        alert('beforeCreate');
+    },
+    created(){
+        alert('created');
+    },
+    beforeMount(){
+        alert('beforeMount');
+    },
+    mounted(){
+        alert('mounted');
+    },
+    beforeUpdate(){
+        alert('beforeUpdate');
+    },
+    updated(){
+        alert('updated');
     }
+
 }
 </script>
 
 <style scoped>
 #osoby{
-  width: 100%;
-  max-width: 1200px;
-  margin: 40px auto;
-  padding: 0 20px;
-  box-sizing: border-box;
+    width: 100%;
+    max-width: 1200px;
+    margin: 40px auto;
+    padding: 0 20px;
+    box-sizing: border-box;
 }
 ul{
-  display: flex;
-  flex-wrap: wrap;
-  list-style-type: none;
-  padding: 0;
+    display: flex;
+    flex-wrap: wrap;
+    list-style-type: none;
+    padding: 0;
 }
 li{
-  flex-grow: 1;
-  flex-basis: 100px;
-  text-align: center;
-  padding: 30px;
-  border: 1px solid #222;
-  margin: 10px;
+    flex-grow: 1;
+    flex-basis: 100px;
+    text-align: center;
+    padding: 30px;
+    border: 1px solid #222;
+    margin: 10px;
 }
 </style>
