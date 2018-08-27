8fd2dfd1e8badf3230a6e0f0c5acdc56f7205a72
marek2222 <marekmusial2@o2.pl>
2018-08-27 06:45:31 +0200

    vuejs2-playlist: 02.27:  Slots - first

---
+++ vuejs2-playlist/27_Slots/src/components/formHelper.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <slot name='tytul'></slot>
+      <h1 >Jestem  "form helper"</h1>
+      <slot name="tekst"></slot>
+
+  </div>
+</template>
+
+<script>
+export default {
+    components: {
+
+    },
+    data () {
+        return {
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+
+<style scoped>
+/* h1{
+
+} */
+</style>
