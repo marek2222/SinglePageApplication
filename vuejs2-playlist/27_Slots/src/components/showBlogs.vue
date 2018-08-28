9bc694b3047d36187ef25f29f6e6ca80ccd1acbb
marek2222 <marekmusial2@o2.pl>
2018-08-28 13:31:49 +0200

    vuejs2-playlist: 02.33: Get Requests. 		Base on the 27_Slots.

---
+++ vuejs2-playlist/27_Slots/src/components/showBlogs.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="show-blogs">
+        <h1>Wszystkie artykuły bloga</h1>
+        <div v-for="blog in blogi" v-bind:key="blog.id" class="single-blog">
+            <h2>{{ blog.title }}</h2>
+            <article>{{ blog.body }}</article>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            blogi: []
+        }
+    },
+    methods: {
+
+    },
+    created() {
+        this.$http.get('http://jsonplaceholder.typicode.com/posts').then(function(data){
+            this.blogi = data.body.slice(0,10);
+        });
+    }
+}
+</script>
+
+<style>
+#show-blogs{
+    max-width: 800px;
+    margin: 0px auto;
+}
+.single-blog{
+    padding: 20px;
+    margin: 20px 0;
+    box-sizing: border-box;
+    background: #eee;
+}
+</style>
